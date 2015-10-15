ef749f27a0a4c79348000dd16ad8c6540904de8d
wenxiang <zhouwenxiang@zivoo.com>
2015-10-15 10:54:22 +0800

    Add android support ViewDragHelper test;

---
+++ app/src/main/java/com/github/neowen/apibasedemo/support/ViewDragHelperA.java
@@ -0,0 +1,9 @@
+package com.github.neowen.apibasedemo.support;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ZWX on 15/10/15.
+ */
+public class ViewDragHelperA extends AppCompatActivity {
+}
