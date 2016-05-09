2d2bc7c7d9fdc7f5c141766bc5ea3155150630da
wenxiang <zhouwenxiang@km.com>
2016-05-09 15:59:33 +0800

    Update some file;

--- app/src/main/java/com/github/neowen/apibasedemo/support/ViewDragHelperA.java
+++ app/src/main/java/com/github/neowen/apibasedemo/support/ViewDragHelperA.java
@@ -2,6 +2,9 @@ package com.github.neowen.apibasedemo.support;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.github.neowen.apibasedemo.R;
 
@@ -10,9 +13,29 @@ import com.github.neowen.apibasedemo.R;
  */
 public class ViewDragHelperA extends AppCompatActivity {
 
+    boolean mHidden;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.viewdraghelper);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                View decorView = getWindow().getDecorView();
+                int uiOptions = mHidden ? View.SYSTEM_UI_FLAG_VISIBLE : View.SYSTEM_UI_FLAG_FULLSCREEN;
+                mHidden = !mHidden;
+                decorView.setSystemUiVisibility(uiOptions);
+
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+//                ActionBar actionBar = getActionBar();
+//                actionBar.hide();
+                Toast.makeText(ViewDragHelperA.this, "A", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
