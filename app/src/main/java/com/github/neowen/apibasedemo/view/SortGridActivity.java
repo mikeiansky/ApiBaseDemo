604338f2e8da6c12b0caf5f3140fcf87e1e4e929
wenxiang <929566854@qq.com>
2018-07-17 19:08:09 +0800

    update sort grid layout;

--- app/src/main/java/com/github/neowen/apibasedemo/view/SortGridActivity.java
+++ app/src/main/java/com/github/neowen/apibasedemo/view/SortGridActivity.java
@@ -23,32 +23,36 @@ public class SortGridActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sort_grid);
         sortGridLayout = (SortGridLayout) findViewById(R.id.sort_grid_layout);
-//        sortGridLayout.setData(Arrays.asList("zhou"
-//                ,"wen 1"
-//                ,"wen 2"
-//                ,"wen 3"
-//                ,"wen 4"
-//                ,"wen 5"
-//                ,"wen 6"
-//                ,"wen 7"
-//                ,"wen 8"
-//                ,"wen 9"
-//                ,"wen 10"
-//                ,"wen 11"
-//                ,"wen 12"
-//                ,"wen 13"
-//                ,"wen 14"
-//                ,"wen 15"
-//                ,"wen 16"
-//                ,"wen 17"
-//                ,"wen 18"
-//                ,"wen 19"
-//                ,"wen 20"
-//                ,"wen 21"));
+        sortGridLayout.setData(Arrays.asList("zhou"
+                , "wen 1"
+                , "wen 2"
+                , "wen 3"
+                , "wen 4"
+                , "wen 5"
+                , "wen 6"
+                , "wen 7"
+                , "wen 8"
+                , "wen 9"
+                , "wen 10"
+                , "wen 11"
+                , "wen 12"
+                , "wen 13"
+                , "wen 14"
+                , "wen 15"
+                , "wen 16"
+                , "wen 17"
+                , "wen 18"
+                , "wen 19"
+                , "wen 20"
+                , "wen 21"));
+
+
         sortGridLayout.setOnItemClickListener(new SortGridLayout.OnItemClickListener() {
             @Override
             public void onItemClick(int position, String item) {
+
                 Toast.makeText(SortGridActivity.this, item, Toast.LENGTH_SHORT).show();
+
             }
         });
 
@@ -63,6 +67,41 @@ public class SortGridActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 sortGridLayout.pageDown();
+            }
+        });
+
+        findViewById(R.id.update_data).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sortGridLayout.setData(Arrays.asList("zhou"
+                        , "xiang 1"
+                        , "xiang 2"
+                        , "xiang 3"
+                        , "xiang 4"
+                        , "xiang 5"
+                        , "xiang 6"
+                        , "xiang 7"
+                        , "xiang 8"
+                        , "xiang 9"
+                        , "xiang 10"
+                        , "xiang 11"
+                        , "xiang 12"
+                        , "xiang 13"
+                        , "xiang 14"
+                        , "xiang 15"
+                        , "xiang 16"
+                        , "xiang 17"
+                        , "xiang 18"
+                        , "xiang 19"
+                        , "xiang 20"
+                        , "xiang 21"));
+            }
+        });
+
+        findViewById(R.id.show).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sortGridLayout.setVisibility(sortGridLayout.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
             }
         });
 
