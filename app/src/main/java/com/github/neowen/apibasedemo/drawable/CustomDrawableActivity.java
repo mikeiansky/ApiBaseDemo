52a65585e9e20b99e5e1ee35ff72c11d59515880
wenxiang <929566854@qq.com>
2018-07-29 14:49:10 +0800

    Update;

--- app/src/main/java/com/github/neowen/apibasedemo/drawable/CustomDrawableActivity.java
+++ app/src/main/java/com/github/neowen/apibasedemo/drawable/CustomDrawableActivity.java
@@ -1,16 +1,22 @@
 package com.github.neowen.apibasedemo.drawable;
 
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.StateListDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.github.neowen.apibasedemo.BaseActivity;
 import com.github.neowen.apibasedemo.R;
+
+import java.io.File;
 
 /**
  * Created by zwx on 2018/7/29.
@@ -20,7 +26,7 @@ public class CustomDrawableActivity extends BaseActivity {
 
     public static final String TAG = CustomDrawableActivity.class.getSimpleName();
 
-    ImageView image,customImage;
+    ImageView image,customImage,loadImage;
     Drawable customDrawable;
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
@@ -31,6 +37,7 @@ public class CustomDrawableActivity extends BaseActivity {
 
         image = (ImageView) findViewById(R.id.image);
         customImage = (ImageView) findViewById(R.id.custom_image);
+        loadImage = (ImageView) findViewById(R.id.load);
         image.setBackground(getResources().getDrawable(R.drawable.btn));
         image.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,7 +47,7 @@ public class CustomDrawableActivity extends BaseActivity {
         });
 
 
-        customImage.setBackground(createCustomDrawable());
+        customImage.setBackground(createMyDrawable());
         customImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,6 +55,31 @@ public class CustomDrawableActivity extends BaseActivity {
             }
         });
 
+//        loadImage.setImageBitmap(BitmapFactory.decodeFile(
+//                Environment.getExternalStorageDirectory().getPath()+ File.separator+"live_s.png"
+//        ));
+
+        loadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "cache dir : " + getCacheDir().getPath());
+
+            }
+        });
+        loadImage.setBackground(createMyDrawable());
+
+    }
+
+    private Drawable createMyDrawable(){
+        Drawable selectDrawable = new BitmapDrawable(getResources(), Environment.getExternalStorageDirectory().getPath()+ File.separator+"live_b.png");
+        Drawable normalDrawable = new BitmapDrawable(getResources(), Environment.getExternalStorageDirectory().getPath()+ File.separator+"live_s.png");
+        StateListDrawable sld = new StateListDrawable();
+        sld.addState(new int[]{android.R.attr.state_focused}, selectDrawable);
+        sld.addState(new int[]{android.R.attr.state_pressed}, selectDrawable);
+        sld.addState(new int[]{android.R.attr.state_selected}, selectDrawable);
+        sld.addState(new int[]{}, normalDrawable);
+
+        return sld;
     }
 
     private Drawable createCustomDrawable() {
