40e94a6a110f2570ab80a96d56660fd458c30354
winson <929566854@qq.com>
2019-01-14 18:22:44 +0800

    Add hybird test;

--- app/src/main/java/com/winson/apibasedemo/hybird/HybirdActivity.kt
+++ app/src/main/java/com/winson/apibasedemo/hybird/HybirdActivity.kt
@@ -1,6 +1,12 @@
 package com.winson.apibasedemo.hybird
 
 import android.os.Bundle
+import android.util.Log
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
 import com.winson.apibasedemo.R
 import com.winson.apibasedemo.base.BaseActivity
 
@@ -10,9 +16,47 @@ import com.winson.apibasedemo.base.BaseActivity
  */
 class HybirdActivity : BaseActivity() {
 
+    val HTML = "<html>\n" +
+            "\n" +
+            "\t<script type=\"text/javascript\">\n" +
+            "\n" +
+            "\t\tfunction callJs(){\n" +
+            "\t\t\tdocument.getElementById(\"content\").innerHTML += \"<br\\>Android 调用了JS函数\"\n" +
+            "\t\t}\n" +
+            "\n" +
+            "\t</script>\n" +
+            "\n" +
+            "\t<body>\n" +
+            "\t\t<button onclick=\"javascript:callJs()\">From Android</button>\n" +
+            "\t\t<br/>\n" +
+            "\t\t<button onclick=\"window.winson.callAndroid('From WebView')\">Call Android</button>\n" +
+            "\t\t<br/>\n" +
+            "\t\t<div id=\"content\">Hello Android</div>\n" +
+            "\n" +
+            "\t</body>\n" +
+            "\n" +
+            "</html>"
+
     override fun onBind(savedInstanceState: Bundle?) {
         super.onBind(savedInstanceState)
         setContentView(R.layout.act_hybird)
+
+        val callJs = findViewById<Button>(R.id.call_js)
+        val webView = findViewById<WebView>(R.id.web_view)
+        webView.settings.javaScriptEnabled = true
+        webView.webChromeClient = WebChromeClient()
+        webView.webViewClient = object : WebViewClient(){}
+        webView.addJavascriptInterface(this, "winson")
+        webView.loadData(HTML, "text/html", "UTF-8")
+        callJs.setOnClickListener {
+            webView.loadUrl("javascript:callJs()")
+        }
+
+    }
+
+    @JavascriptInterface
+    fun callAndroid(msg: String) {
+        Log.d("TAG", "callAndroid -------> $msg")
     }
 
 }
